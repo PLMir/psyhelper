fe1f7eb8cda61029723028fbc97f09c1154630eb
PLMir <mirabile.patricia@gmail.com>
2019-10-20 19:59:08 +0200

    licensing and documentation

--- R/get_ordinal_priors.R
+++ R/get_ordinal_priors.R
@@ -1,7 +1,31 @@
+#' Get priors for an ordinal variable converted into cumulative log odds
+#'
+#' @param probs A vector of probabilities for each threshold (i.e response point) on your scale.
+#'   Probabilities must sum to 1 or to 100.
+#'
+#' @return A vector of cumulative log odds for all n-1 thresholds (i.e. response points).
+#'   The cumulative log odds for the nth threshold are not returned, given that :
+#'   1) the log odds for the last threshold does not need to be specified as a prior in bmrs;
+#'   2) probabilities nessarily sum to 1 and the log odds for 1 is Inf.
+#' @export
+#'
+#' @examples
+#' # You may specify the probabilities as percentages.
+#' get_ordinal_priors(5, 20, 10, 15, 25, 25) # -2.9444390 -1.0986123 -0.6190392  0.0000000  1.0986123
+#'
+#' # You may also specify the probabilities on a 0 to 1 scale.
+#' get_ordinal_priors(.05, .2, .1, .15, .25, .25)
+#'
+#' \dontrun{
+#' # These will return an error because those probabilities do not sum to 100 or to 1
+#' get_ordinal_priors{c(50, 45)
+#' get_ordinal_priors{c(.5, .45)
+#' }
+
 get_ordinal_priors <- function(probs) {
+  if(!is.numeric(probs)) (stop("Please specify a vector containing only numeric values."))
   n <- length(probs)
   if(n < 2) (warning("Please specify the probability for at least two thresholds."))
-  if(!is.numeric(probs)) (stop("Please specify a vector containing only numeric values."))
 
   sum_probs <- sum(probs)
 
--- R/read_qualtrics.R
+++ R/read_qualtrics.R
@@ -1,3 +1,25 @@
+#' Use read_csv on files downloaded from Qualtrics
+#'
+#' Data files exported from Qualtrics are famously difficult to import in R as the
+#' first three rows contain column names, text from the prompts and question labels.
+#' The second row contains the most user friendly information, but because it is not possible
+#' to skip both the first and the third row when reading a file, this usually leads to the
+#' annoying consequence that your first row contains strings, which are either converted to factors
+#'  or force you to specify the type for all columns.
+#'  Use \code{read_qualtrics} instead to directly import your csv file with usable column names and
+#'  allow \code{read_csv} to impute your column types.
+#'
+#' @param A path to a csv file imported from Qualtrics
+#'
+#' @return A tibble().
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' read_qualtrics("qualtrics_import.csv")
+#' }
+#'
+
 read_qualtrics <- function(file) {
   col_names <- file %>%
     readr::read_csv(col_names = FALSE, n_max = 1) %>%
